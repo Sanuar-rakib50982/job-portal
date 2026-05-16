49ce8f513685fa3636c7af088c66643de13d1e96
safin-55 <mohammadsafin34@gmail.com>
2026-05-16 12:52:25 +0600

    Add recruiter client management methods

--- app/controllers/RecruiterController.php
+++ app/controllers/RecruiterController.php
@@ -55,4 +55,72 @@ class RecruiterController {
 
         return $stmt->execute();
     }
+
+    public function getClients($recruiterId) {
+    $sql = "SELECT * FROM recruiter_clients WHERE recruiter_id = ? ORDER BY created_at DESC";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param("i", $recruiterId);
+    $stmt->execute();
+
+    return $stmt->get_result();
+}
+
+public function getClientById($clientId, $recruiterId) {
+    $sql = "SELECT * FROM recruiter_clients WHERE id = ? AND recruiter_id = ?";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param("ii", $clientId, $recruiterId);
+    $stmt->execute();
+
+    return $stmt->get_result()->fetch_assoc();
+}
+
+public function createClient($recruiterId, $data) {
+    $sql = "INSERT INTO recruiter_clients
+            (recruiter_id, company_name, contact_person, email, phone, industry, address)
+            VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(
+        "issssss",
+        $recruiterId,
+        $data['company_name'],
+        $data['contact_person'],
+        $data['email'],
+        $data['phone'],
+        $data['industry'],
+        $data['address']
+    );
+
+    return $stmt->execute();
+}
+
+public function updateClient($clientId, $recruiterId, $data) {
+    $sql = "UPDATE recruiter_clients
+            SET company_name = ?, contact_person = ?, email = ?, phone = ?, industry = ?, address = ?
+            WHERE id = ? AND recruiter_id = ?";
+
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(
+        "ssssssii",
+        $data['company_name'],
+        $data['contact_person'],
+        $data['email'],
+        $data['phone'],
+        $data['industry'],
+        $data['address'],
+        $clientId,
+        $recruiterId
+    );
+
+    return $stmt->execute();
+}
+
+public function deleteClient($clientId, $recruiterId) {
+    $sql = "DELETE FROM recruiter_clients WHERE id = ? AND recruiter_id = ?";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param("ii", $clientId, $recruiterId);
+
+    return $stmt->execute();
+}
+
 }
